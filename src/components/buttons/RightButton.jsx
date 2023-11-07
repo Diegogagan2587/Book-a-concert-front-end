2b54d6ea4439a2fe19ee4f5c8e7005954310305b
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-07 14:29:52 -0600

    Create button: Right button

---
+++ src/components/buttons/RightButton.jsx
@@ -0,0 +1,16 @@
+function RightButton() {
+    return (
+      <button
+        className="flex items-center justify-end text-xl w-16 md:w-20 py-1 
+    bg-[#94bc0c] border-white border-solid border-2 text-white
+  rounded-l-xl md:rounded-l-2xl px-4
+  hover:bg-slate-100 hover:text-[#94bc0c] hover:border-[#94bc0c]
+  transition duration-500 ease-in-out"
+      >
+        <ion-icon name="chevron-back-outline"></ion-icon>
+      </button>
+    );
+  }
+  
+  export default RightButton;
+  
