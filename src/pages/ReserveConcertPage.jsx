11ce48b2c7f06156f3fc2b3a221a5f827beeb1a9
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-08 10:51:20 -0600

    Add Title and description placeholders

--- src/pages/ReserveConcertPage.jsx
+++ src/pages/ReserveConcertPage.jsx
@@ -1,8 +1,16 @@
 function ReserverConcertPage() {
   return (
-    <div>
-      <h1>ReserverConcertPage</h1>
-    </div>
+    <main className="bg-[#96bf01] text-white h-screen w-full flex flex-col items-center justify-center">
+      <section className="text-center flex flex-col items-center md:w-1/2 ">
+        <h1 className="text-2xl">Book for a concert: get your ticket now!</h1>
+        <hr className="w-full" />
+        <p>
+          There are many Independent Concerts that you can book for. Any
+          Independent artist can create an event or concert in our platform and
+          you can book for it. Support your favorite artist!
+        </p>
+      </section>
+    </main>
   );
 }
 
