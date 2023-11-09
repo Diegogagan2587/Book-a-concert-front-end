85a36c4c137667550a91da80dbd8ed293d6e76be
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-09 13:51:47 -0600

    Add dummy data as initial state for displayin on ItemDetails panel component

--- src/redux/concertDetails/concertDetailsSlice.js
+++ src/redux/concertDetails/concertDetailsSlice.js
@@ -1,22 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+//Placeholder State: remove after connecting to backend
+const initialState = {
+  id: 1,
+  title: 'Test - Ariana Grande Live Concert',
+  organizer: 'Test - Mr. Satan',
+  description:
+    'test: Ariana Concert: Ariana Grande is an American singer, songwriter, and actress. A multi-platinum, Grammy Award-winning recording artist, she is known for her wide vocal range, which critics have often compared to that of Mariah Carey.',
+  img: 'https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  price: '100.0',
+  date: '2021-10-10',
+  city: 'Test - New York',
+  created_at: '2023-11-08T05:48:47.047Z',
+  updated_at: '2023-11-08T05:48:47.047Z',
+};
+
 export const concertDetailsSlice = createSlice({
   name: 'concertDetails',
-  initialState: {
-    concertDetails: {},
-  },
+  initialState,
   reducers: {
     //reducers functions here
-    fetchConcert: (state, action) => {
+    fetchedConcert: (state, action) => {
       //logic here example:
       //state.concertDetails = action.payload;
     },
-    reserveConcert: (state, action) => {
+    reservedConcert: (state, action) => {
       //logic here example:
       //state.concertDetails = action.payload;
     },
   },
 });
 
-export const { fetchConcert, reserveConcert } = concertDetailsSlice.actions;
+export const { fetchedConcert, reservedConcert } = concertDetailsSlice.actions;
 export default concertDetailsSlice.reducer;
