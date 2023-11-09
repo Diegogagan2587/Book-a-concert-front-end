7fa52cef1c153ba66b776461f5de8bccc5fa3295
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-08 19:45:53 -0600

    Add Slice Reducers to the Store: concertDetailsreducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import concertDetailsReducer from './concertDetails/concertDetailsSlice';
 
 const store = configureStore({
   reducer: {
     //slices here
+    concertDetails: concertDetailsReducer,
   },
 });
 
