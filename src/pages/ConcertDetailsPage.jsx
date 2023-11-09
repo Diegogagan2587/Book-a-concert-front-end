2dc8d3ef9916c9e3a55022e756c09efa8344e694
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-08 21:23:54 -0600

    User Redux state to display Concert Data

--- src/pages/ConcertDetailsPage.jsx
+++ src/pages/ConcertDetailsPage.jsx
@@ -1,20 +1,24 @@
+import { useSelector } from 'react-redux';
 import ItemDataPanel from '../components/ItemDataPanel';
 import LeftButton from '../components/buttons/LeftButton';
 
-let imgURL =
-  'https://images.pexels.com/photos/1387174/pexels-photo-1387174.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
 function ConcertDetailsPage() {
+  const concert = useSelector((state) => state.concertDetails);
   return (
     // Div below need to be adjusted for thir CSS properties when integrating this component to the app
     //Right now its position is absolute so might intergere with other components
     <section className=" container flex flex-col w-full min-h-screen mx-0 my-0 bg-white">
       <article className=" flex flex-col md:flex-row m-2 p-2 ">
         <div className="flex items-center justify-center h-2/3 border-2 bg-gray-100 rounded rounded-lg p-2 mb-2 md:mb-inherit">
-          <img src={imgURL} alt="concert" className="rounded-lg max-h-full" />
+          <img
+            src={concert.img}
+            alt="concert"
+            className="rounded-lg max-h-full"
+          />
         </div>
 
         <div className="container flex justify-between md:w-1/3">
-          <ItemDataPanel />
+          <ItemDataPanel concert={concert} />
         </div>
       </article>
       <LeftButton />
