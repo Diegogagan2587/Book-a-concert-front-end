239833e901beba6d3ac30cc1940ffeb936a68d15
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-08 19:43:36 -0600

    Fix typo

--- src/redux/concertDetails/concertDetailsSlice.js
+++ src/redux/concertDetails/concertDetailsSlice.js
@@ -1,22 +1,22 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const concertDetailsSlice = createSlice({
-    name: 'concertDetails',
-    initialState: {
-        concertDetails: {}
-    },
-    reducers: {
+  name: 'concertDetails',
+  initialState: {
+    concertDetails: {},
+  },
+  reducers: {
     //reducers functions here
     fetchConcert: (state, action) => {
-        //logic here example:
-        //state.concertDetails = action.payload;
+      //logic here example:
+      //state.concertDetails = action.payload;
     },
     reserveConcert: (state, action) => {
-        //logic here example:
-        //state.concertDetails = action.payload;
+      //logic here example:
+      //state.concertDetails = action.payload;
     },
-})
+  },
+});
 
-
-export const {fetchConcert, reserveConcert} = concertDetailsSlice.actions;
+export const { fetchConcert, reserveConcert } = concertDetailsSlice.actions;
 export default concertDetailsSlice.reducer;
