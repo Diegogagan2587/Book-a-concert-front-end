a9b0d84c1e8013fde500ef3bc83815bf9eb0657a
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-08 19:39:36 -0600

    Create a Redux state slice, include reducers place holders for fetchConcert and reserveConcert

---
+++ src/redux/concertDetails/concertDetailsSlice.js
@@ -0,0 +1,22 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+export const concertDetailsSlice = createSlice({
+    name: 'concertDetails',
+    initialState: {
+        concertDetails: {}
+    },
+    reducers: {
+    //reducers functions here
+    fetchConcert: (state, action) => {
+        //logic here example:
+        //state.concertDetails = action.payload;
+    },
+    reserveConcert: (state, action) => {
+        //logic here example:
+        //state.concertDetails = action.payload;
+    },
+})
+
+
+export const {fetchConcert, reserveConcert} = concertDetailsSlice.actions;
+export default concertDetailsSlice.reducer;
