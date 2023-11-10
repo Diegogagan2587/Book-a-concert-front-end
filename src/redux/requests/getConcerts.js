8a613dacdc852b5012114e6dfabe9ecd75fbe7a2
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-09 21:09:15 -0600

    Create AsyncThunk: getConcerts

---
+++ src/redux/requests/getConcerts.js
@@ -0,0 +1,12 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL_BASE = 'https://book-a-concert-api.onrender.com';
+const GET_CONCERTS_URL = `${API_URL_BASE}/concerts/`;
+
+const getConcerts = createAsyncThunk('getConcerts', async () => {
+  const response = await fetch(GET_CONCERTS_URL);
+  const data = await response.json();
+  return data;
+});
+
+export default getConcerts;
