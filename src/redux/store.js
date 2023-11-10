a02e1672d336133bc9ae03a315e31e868c6e7d5d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-09 21:15:58 -0600

    Add reservationForm reducers to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import reservationFormReducer from './reservationForm/reservationFormSlice';
 
 const store = configureStore({
   reducer: {
     //slices here
+    reservationForm: reservationFormReducer,
   },
 });
 
