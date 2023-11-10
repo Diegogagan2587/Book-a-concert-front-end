29976d8fea02e83e3237eb6f1b1647681a4a1293
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-09 18:10:44 -0600

    Create asynthink: Request to Post a new Reservation to the API

---
+++ src/redux/requests/postReservation.js
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const API_URL_BASE = 'https://book-a-concert-api.onrender.com';
+const POST_RESERVATION_URL = `${API_URL_BASE}/reservations`;
+
+const postReservation = createAsyncThunk(
+  'postReservation',
+  async ({ user_id, concert_id, date, city }) => {
+    const response = await fetch(POST_RESERVATION_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        reservation: {
+          user_id: user_id, // Replace with the actual user's ID
+          concert_id: concert_id, // Replace with the actual concert's ID
+          date: date,
+          city: city,
+        },
+      }),
+    });
+    const data = await response.json();
+    return data;
+  }
+);
+
+export default postReservation;
