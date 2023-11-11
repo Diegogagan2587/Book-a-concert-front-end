f4baf48660efa8e5a756e3eb894e40fb043b24ba
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-11-10 18:30:16 -0600

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slices/userSlice'; 
+import userReducer from '../redux/slices/userSlice'; 
 import concertDetailsReducer from './concertDetails/concertDetailsSlice';
 
 const store = configureStore({
