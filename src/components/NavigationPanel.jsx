c0db8277a87f165a1e733f59a9b17fea4c6b848d
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-11-13 03:19:33 -0600

    Upgrade nav pannel functionality

--- src/components/NavigationPanel.jsx
+++ src/components/NavigationPanel.jsx
@@ -1,24 +1,35 @@
 // src/components/NavigationPanel.jsx
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logoutUser } from '../redux/slices/userSlice';
 
 function NavigationPanel() {
   const user = useSelector((state) => state.user.details);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logoutUser());
+    navigate('/');
   };
 
   return (
     <nav>
       <Link to="/">Main Page</Link>
-      <Link to="/reserve">Reserve</Link>
-      <Link to="/my-reservations">My Reservations</Link>
-      <Link to="/add-concert">Add Concert</Link>
-      <Link to="/delete-concert">Delete Concert</Link>
-      {user && <button onClick={handleLogout}>Logout</button>}
+      {user ? (
+        <>
+          <Link to="/reserve">Reserve</Link>
+          <Link to="/my-reservations">My Reservations</Link>
+          <Link to="/add-concert">Add Concert</Link>
+          <Link to="/delete-concert">Delete Concert</Link>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => navigate('/login')}>Login</button>
+          <button onClick={() => navigate('/signup')}>Signup</button>
+        </>
+      )}
     </nav>
   );
 }
