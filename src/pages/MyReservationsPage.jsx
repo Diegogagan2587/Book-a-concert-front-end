cbf4c2c4641efcfaffa46f4b11755150a11d13f2
dani <123456789+odin@users.noreply.github.com>
2023-11-14 12:25:45 -0500

    Remove user_id from reservations list and Show current_user name when the list is empty

--- src/pages/MyReservationsPage.jsx
+++ src/pages/MyReservationsPage.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 
 function MyReservationsPage() {
   const [myReservations, setMyReservations] = useState([]);
+  const [current_user, setCurrentUser] = useState([]);
 
   useEffect(() => {
     fetch('https://book-a-concert-api.onrender.com/current_user/reservations')
@@ -10,27 +10,38 @@ function MyReservationsPage() {
       .then(data => setMyReservations(data));
   }, []);
 
-  // console.log(myReservations);
+  console.log(myReservations);
+
+  useEffect(() => {
+    fetch('https://book-a-concert-api.onrender.com/current_user')
+      .then(response => response.json())
+      .then(data => setCurrentUser(data));
+  }, []);
+  
 
   return (
     <div className="main-page">
       <h1>My Reservations</h1>
-      <div className='header'>
-        <span>User</span>
-        <span>Concert</span>
-        <span>Date</span>
-        <span>City</span>
-      </div>
-      <ul className="reservations-list">
-        {myReservations.map(reservation => (
-          <li key={reservation.id} className="reservation-item">
-            <span>{reservation.user_id}</span>___
-            <span>{reservation.concert_id}</span>___
-            <span>{reservation.date}</span>___
-            <span>{reservation.city}</span>
-          </li>
-        ))}
-      </ul>
+      {myReservations.length === 0 ? (
+        <h2>The user <strong>{current_user.name}</strong> has no reservations yet</h2>
+      ) : (
+        <div className='header'>
+          <span>Concert</span>
+          <span>Date</span>
+          <span>City</span>
+        </div>
+      )}
+      {myReservations.length > 0 && (
+        <ul className="reservations-list">
+          {myReservations.map(reservation => (
+            <li key={reservation.id} className="reservation-item">
+              <span>{reservation.concert_title}</span>___
+              <span>{reservation.date}</span>___
+              <span>{reservation.city}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
