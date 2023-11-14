eb3f70bd351593bcedaec89c2c71f2282ef06082
dani <123456789+odin@users.noreply.github.com>
2023-11-14 13:17:49 -0500

    Add more details to each my-reservation like: Image, Description

--- src/pages/MyReservationsPage.jsx
+++ src/pages/MyReservationsPage.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import '../stylesheets/MyReservationsPage.css';
 
 function MyReservationsPage() {
   const [myReservations, setMyReservations] = useState([]);
   const [current_user, setCurrentUser] = useState([]);
+  const [concerts, setConcerts] = useState([]);
 
   useEffect(() => {
     fetch('https://book-a-concert-api.onrender.com/current_user/reservations')
@@ -10,36 +12,42 @@ function MyReservationsPage() {
       .then(data => setMyReservations(data));
   }, []);
 
-  console.log(myReservations);
-
   useEffect(() => {
     fetch('https://book-a-concert-api.onrender.com/current_user')
       .then(response => response.json())
       .then(data => setCurrentUser(data));
   }, []);
-  
+
+  useEffect(() => {
+    fetch('https://book-a-concert-api.onrender.com/concerts')
+      .then(response => response.json())
+      .then(data => setConcerts(data));
+  }, []);
 
   return (
-    <div className="main-page">
+    <div className="my-reservations-page">
       <h1>My Reservations</h1>
       {myReservations.length === 0 ? (
         <h2>The user <strong>{current_user.name}</strong> has no reservations yet</h2>
       ) : (
-        <div className='header'>
-          <span>Concert</span>
-          <span>Date</span>
-          <span>City</span>
-        </div>
-      )}
-      {myReservations.length > 0 && (
-        <ul className="reservations-list">
-          {myReservations.map(reservation => (
-            <li key={reservation.id} className="reservation-item">
-              <span>{reservation.concert_title}</span>___
-              <span>{reservation.date}</span>___
-              <span>{reservation.city}</span>
-            </li>
-          ))}
+        <ul>
+          {myReservations.map(reservation => {
+            const concert = concerts.find(concert => concert.id === reservation.concert_id);
+            const img = concert ? concert.img : 'Image not available';
+            const description = concert ? concert.description : 'Description not available';
+
+            return (
+              <li key={reservation.id} >
+                <img src={img} alt="Concert Image" />
+                <div className='bottom'>
+                  <div>{reservation.concert_title}</div>
+                  <div>{description}</div>
+                  <div>{reservation.city}</div>
+                  <div>{reservation.date}</div>
+                </div>
+              </li>
+            );
+          })}
         </ul>
       )}
     </div>
