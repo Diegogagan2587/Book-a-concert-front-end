c920be3a192e1033e01b4d0abf2d274904aa63ae
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-14 22:19:48 -0600

    Implement First version of a caourusel

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,31 +1,46 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import LeftButton from '../components/buttons/LeftButton';
+import RightButton from '../components/buttons/RightButton';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function MainPage() {
   const [concerts, setConcerts] = useState([]);
 
   useEffect(() => {
     fetch('https://book-a-concert-api.onrender.com/concerts')
-      .then(response => response.json())
-      .then(data => setConcerts(data));
+      .then((response) => response.json())
+      .then((data) => setConcerts(data));
   }, []);
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Adjust the number of slides to show
+    slidesToScroll: 1,
+    prevArrow: <LeftButton />,
+    nextArrow: <RightButton />,
+  };
+
   return (
-    <div className="main-page">
+    <div className="main-page container">
       <h1>Available Concerts</h1>
-      <ul className="concert-list">
-        {concerts.map(concert => (
-          <li key={concert.id}>
+      <Slider {...settings}>
+        {concerts.map((concert) => (
+          <div key={concert.id}>
             <Link to={`/concerts/${concert.id}`}>
               <div className="concert-item">
-                <img src={concert.img} alt={concert.title} />
+                <img src={concert.img} alt={concert.title} className="w-md" />
                 <h2>{concert.title}</h2>
                 <p>{concert.description}</p>
               </div>
             </Link>
-          </li>
+          </div>
         ))}
-      </ul>
+      </Slider>
     </div>
   );
 }
