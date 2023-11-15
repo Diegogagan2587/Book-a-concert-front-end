2e4ffc8a82cc49a315e3626b2d2c243c54fd1e6b
dani <123456789+odin@users.noreply.github.com>
2023-11-14 19:12:50 -0500

    Add logic to manage log in action

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,45 @@
-// src/components/Login.jsx
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../redux/slices/userSlice';
+import { useNavigate } from 'react-router-dom';
+import '../stylesheets/Login.css';
 
 const Login = () => {
   const [username, setUsernameInput] = useState('');
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const userStatus = useSelector((state) => state.user.status);
-  const userMessage = useSelector((state) => state.user.details.message); // New line
+  const userMessage = useSelector((state) => state.user.details.message);
 
   const handleLogin = () => {
     dispatch(loginUser(username));
   };
 
-  return (
-    <div>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsernameInput(e.target.value)}
-        placeholder="Username"
-      />
-      <button onClick={handleLogin}>Login</button>
-      {userStatus === 'loading' && <p>Loading...</p>}
-      {userStatus === 'failed' && <p>Error: {userMessage || 'User not found'}</p>}
-      {userStatus === 'succeeded' && <p>{userMessage}</p>}
+  useEffect(() => {
+    if (userStatus === 'succeeded') {
+      navigate('/');
+    }
+  }, [userStatus, navigate]);
 
+  return (
+    <div className='login-page'>
+      <h1>Book a Concert App</h1>
+      <div className='login-container'>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsernameInput(e.target.value)}
+          placeholder="Username"
+          className='login-input'
+          required
+        />
+        <button onClick={handleLogin} className='btn'>Login</button>
+        {userStatus === 'failed' && (
+          <div className='error'>
+            <p>Error: {userMessage || 'User not found'}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
