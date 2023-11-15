fdf2ec27916858886c09f274507e9a1ccb70af65
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-14 19:32:50 -0600

    Fix Error within concert details page: Organizer data preventing rendering of the page

--- src/components/ItemDataPanel.jsx
+++ src/components/ItemDataPanel.jsx
@@ -23,7 +23,7 @@ function ItemDataPanel(props) {
             <tbody>
               <tr className="bg-neutral-100">
                 <td>Organized by:</td>
-                <td>{organizer.name}</td>
+                <td>{organizer&&organizer.name}</td>
               </tr>
               <tr>
                 <td>Date:</td>
@@ -55,7 +55,7 @@ function ItemDataPanel(props) {
 ItemDataPanel.propTypes = {
   concert: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    organizer: PropTypes.string.isRequired,
+    organizer: PropTypes.string,
     description: PropTypes.string.isRequired,
     price: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
