700ee6edf870900d2b71cd0d1d846def6a30f33e
Diego Vidal Lopez <diego27796gagan@gmail.com>
2023-11-16 18:04:34 -0600

    create AsyncThunk: getReservations in getReservations.js

---
+++ src/redux/requests/getReservations.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const API_URL_BASE = "https://book-a-concert-api.onrender.com";
+const GET_RESERVATIONS_URL = `${API_URL_BASE}/reservations`;
+
+const getReservations = createAsyncThunk("getReservations", async () => {
+    const response = await fetch(GET_RESERVATIONS_URL);
+    const data = await response.json()
+    await ('getReservations request response', data )
+    return data;
+});
+
+export default getReservations;
