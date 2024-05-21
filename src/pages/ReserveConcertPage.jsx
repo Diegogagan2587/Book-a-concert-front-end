bb20522eda5477fc84ae28c836ee330002a27ce7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-20 20:33:44 -0600

    feat/reserveConcertPate(Form): Set as placeholder pre-selected values or selection place holders

--- src/pages/ReserveConcertPage.jsx
+++ src/pages/ReserveConcertPage.jsx
@@ -82,7 +82,7 @@ function ReserveConcertPage() {
                   id="select-city"
                   name="select-city"
                   items={availableCities}
-                  placeHolder={'Select City'}
+                  placeHolder={ preSelected && preSelected.city || 'Select City' }
                   onChange={(e) =>
                     dispatch(updatedAvailability(e.target.value))
                   }
@@ -93,7 +93,7 @@ function ReserveConcertPage() {
                   id="select-date"
                   name="select-date"
                   items={availableDates}
-                  placeHolder={'Select Date'}
+                  placeHolder={preSelected && preSelected.date || 'Select Date'}
                   onChange={(e) =>
                     dispatch(updatedAvailability(e.target.value))
                   }
@@ -104,7 +104,7 @@ function ReserveConcertPage() {
                   id="select-concert"
                   name="select-concert"
                   items={availableConcerts}
-                  placeHolder={'Select Concert'}
+                  placeHolder={preSelected && preSelected.title || 'Select Concert'}
                   onChange={(e) =>
                     dispatch(updatedAvailability(e.target.value))
                   }
