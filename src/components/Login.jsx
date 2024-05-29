bce05b0615617e8b279fa177924873dc263e7c9d
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-05-28 23:32:26 -0600

    feat/login: add svg background

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loginUser } from '../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
 import '../stylesheets/Login.css';
+import concertSVG from '../assets/concert-icon.svg';
 import Loading from './Loading';
 
 const Login = () => {
@@ -23,36 +24,37 @@ const Login = () => {
   }, [userStatus, navigate]);
 
   return (
-    <div className='login-page'>
-      <h1 className='font-bold'>BOOK A CONCERT APP</h1>
-      <div className='login-container'>
-        <input
-          type="email"
-          value={user.email}
-          onChange={(e) => setUser({...user, email: e.target.value})}
-          placeholder="user@mail.com"
-          className='login-input'
-          required
-        />
-        <input
-        type='password'
-        value={user.password}
-        onChange={(e) => setUser({...user, password: e.target.value})}
-        placeholder="Password"
-        className='login-input'
-        required
-        />
-        <button onClick={handleLogin} className="btn">
-          Login
-        </button>
-        {userStatus === 'loading' && (
-          <Loading />
-        )}
-        {userStatus === 'failed' && (
-          <div className='error'>
-            <p>{userMessage || 'User not found'}</p>
-          </div>
-        )}
+    <div className="login-page">
+      <img src={concertSVG} alt="concert icon" className="fixed z-0 w-1/2" />
+      <div className="z-10 flex flex-col items-center">
+        <h1 className="font-bold text-center">BOOK A CONCERT APP</h1>
+        <div className="login-container">
+          <input
+            type="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            placeholder="user@mail.com"
+            className="login-input"
+            required
+          />
+          <input
+            type="password"
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            placeholder="Password"
+            className="login-input"
+            required
+          />
+          <button onClick={handleLogin} className="btn">
+            Login
+          </button>
+          {userStatus === 'loading' && <Loading />}
+          {userStatus === 'failed' && (
+            <div className="error">
+              <p>{userMessage || 'User not found'}</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
