a4d7d8b7538cd40e80c0d848e1eba616556b672c
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-07-21 10:02:02 -0600

    docs/readme: add reference to login background img author

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,7 @@ import { loginUser } from '../redux/slices/userSlice';
 import { useNavigate } from 'react-router-dom';
 import '../stylesheets/Login.css';
 import concertSVG from '../assets/concert-icon.svg';
+import concertBG from '../assets/img/concert-bg-1.jpg';
 import Loading from './Loading';
 
 const Login = () => {
@@ -25,7 +26,7 @@ const Login = () => {
 
   return (
     <div className="login-page">
-      <img src={concertSVG} alt="concert icon" className="fixed z-0 w-1/2" />
+      <img src={concertBG} alt="concert icon" className="fixed z-0 w-1/2" />
       <div className="z-10 flex flex-col items-center">
         <h1 className="font-bold text-center font-sans">BOOK A CONCERT APP</h1>
         <div className="login-container">
