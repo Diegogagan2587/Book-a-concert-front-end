a5a2334c7aa26688100c8e61dd3e2c40901dfdf7
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-07-21 18:42:30 -0600

    refact/Login: renders Please fill in all fields if a field is missing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,13 +8,19 @@ import Loading from '../components/Loading';
 
 const Login = () => {
   const [user, setUser] = useState({ email: '', password: '' });
+  const [validationError, setValidationError] = useState('');
   const userStatus = useSelector((state) => state.user.status);
   const userMessage = useSelector((state) => state.user.details.message);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    dispatch(loginUser(user));
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if(user.email && user.password){
+      dispatch(loginUser(user));
+    } else {
+      setValidationError('Please fill in all fields');
+    }
   };
 
   useEffect(() => {
@@ -25,33 +31,34 @@ const Login = () => {
 
   return (
     <AuthContainer>
-        <form className="login-container">
-          <input
-            type="email"
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-            placeholder="user@mail.com"
-            className="login-input"
-            required
-          />
-          <input
-            type="password"
-            value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            placeholder="Password"
-            className="login-input"
-            required
-          />
-          <button type='submmit' onClick={handleLogin} className="btn">
-            Login
-          </button>
-          {userStatus === 'loading' && <Loading />}
-          {userStatus === 'failed' && (
-            <div className="error">
-              <p>{userMessage || 'User not found'}</p>
-            </div>
-          )}
-        </form>
+      <form className="login-container">
+        <input
+          type="email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="user@mail.com"
+          className="login-input"
+          required
+        />
+        <input
+          type="password"
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+          className="login-input"
+          required
+        />
+        <button type="submmit" onClick={handleLogin} className="btn">
+          Login
+        </button>
+        {validationError && <p className="error">{validationError}</p>}
+        {userStatus === 'loading' && <Loading />}
+        {userStatus === 'failed' && (
+          <div className="error">
+            <p>{userMessage || 'User not found'}</p>
+          </div>
+        )}
+      </form>
     </AuthContainer>
   );
 };
