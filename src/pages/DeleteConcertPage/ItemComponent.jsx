a008b508aa0fcac29fe84984d1aa68848b91cea3
Diego Vidal Lopez <diego27796gagan@gmail.com>
2024-07-24 16:58:51 -0600

    refact/DelteConcertPage(ItemComponent.jsx): accepts handle delete as prop

--- src/pages/DeleteConcertPage/ItemComponent.jsx
+++ src/pages/DeleteConcertPage/ItemComponent.jsx
@@ -1,14 +1,12 @@
 import { PropTypes } from 'prop-types';
 
-const ItemComponent = ({concert}) => {
+const ItemComponent = ({concert, handleDelete}) => {
     const { title, id } = concert;
-    const handleDelete = (id) => {
-      console.log(`Deleting concert with id: ${id}`);
-    };
+
     return (
       <li>
         <span>{title}</span>
-        <button onClick={()=> handleDelete(id)}>Delete</button>
+        <button onClick={()=>handleDelete(id)}>Delete</button>
       </li>
     );
   };
